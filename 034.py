32e20d50f1ad9482a093f582456e830ca2f5d9bf
Vladimir Khromov <vlad2aria@mail.ru>
2023-02-06 23:50:03 +0300

    34 problem

--- 034.py
+++ 034.py
@@ -4,19 +4,23 @@ Find the sum of all numbers which are equal to the sum of the factorial of their
 Note: As 1! = 1 and 2! = 2 are not sums they are not included.
 """
 from math import prod
+
+
 def factorial(number: int) -> int:
-    return prod(range(1, number+1))
+    return prod(range(1, number + 1))
+
 
 assert factorial(5) == 120, "fac5"
 assert factorial(4) == 24, "fac4"
 assert factorial(1) == 1, "fac1"
 assert factorial(2) == 2, "fac2"
 
+
 def is_sum_fac(number: int) -> bool:
     str_num = list(map(int, str(number)))
     return number == sum([factorial(i) for i in str_num])
 
 
-result = [i for i in range(3, 10**6) if is_sum_fac(i)]
+result = [i for i in range(3, 10 ** 6) if is_sum_fac(i)]
 
 print(f"The sum of all numbers which are equal to the sum of the factorial of their digits is {sum(result)}")
